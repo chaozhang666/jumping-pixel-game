6af2a98991cc179ffddc3654d5cfaf836a11b196
Chao Zhang <raidenchao@gmail.com>
2020-10-26 00:22:22 -0700

    项目搁置

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -2,23 +2,47 @@
   <div class="container">
     <Menu />
     <div class="content">
-      <h1>Play</h1>
+      <canvas id="myCanvas"></canvas>
     </div>
-    
+
   </div>
 </template>
 
 <script>
 import Menu from '../components/Menu'
-
 export default {
   name: 'Play',
+  data() {
+    return {
+      birdX: 200,
+      birdY: 200,
+      birdSize: 200
+    }
+  },
+  methods: {
+    // drawBird() {
+    //   ctx.beginPath();
+    //   ctx.rect(birdX, canvas.height - birdSize - birdY, birdSize, birdSize);
+    //   ctx.fillStyle = "yellow";
+    //   ctx.fill();
+    //   ctx.closePath();
+    // }
+  },
+  mounted() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.rect(this.birdX, 2000 - this.birdSize - this.birdY, this.birdSize, this.birdSize);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+
+  },
   components: {
     Menu
   }
 }
 </script>
-
 
 <style scoped>
 .container {
@@ -34,5 +58,11 @@ export default {
   justify-content: center;
   align-items: center;  
   background: #c62a88; 
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
 }
 </style>
